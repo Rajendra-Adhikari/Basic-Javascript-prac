93b26d1c9570d2b0a92d999bc106b89064d3ed8f
Rajendra-Adhikari <adhikarirajendra237@gmail.com>
2025-06-29 16:53:30 +0545

    raj-jsprac

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 //make toogle button that changes the screen to dark mode when clicked
 //and light mode when clicked again
-
 let modeBtn=document.querySelector("#mode");
-let currentmode="light";//darl
+let body=document.querySelector("body")
+let currentmode="light";//dark
 modeBtn.addEventListener("click",()=>{
     if(currentmode==="light"){
         currentmode="dark";
-    document.querySelector("body").style.backgroundColor="black";
+    body.classList.add("dark");
+    body.classList.remove("light");//for applied second time
+    
     }
     else{
         currentmode="light";
-    document.querySelector("body").style.backgroundColor="white";
+    body.classList.add("light");
+    body.classList.remove("dark");
+    
     }
     console.log(currentmode);
 });
